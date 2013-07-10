8de8c59789360daea71c20996ee142c110e8c91a
romeyke <romeyke@c63e1d0e-0205-4395-96c5-23d0ee883611>
2013-07-10 11:43:22 +0000

    - refactored, renamed 'flag_be_quiet' to 'flag_be_verbose' to avoid misunderstandings
    git-svn-id: https://sdvsvn10.slub-dresden.de/svn/Langzeitarchivierung/trunk/fixit/fixit_tiff@345 c63e1d0e-0205-4395-96c5-23d0ee883611

--- fixit_tiff.c
+++ fixit_tiff.c
@@ -15,7 +15,7 @@
 #define TIFFDATETIMELENGTH 20
 
 /** global variables */
-static int flag_be_quiet=0;
+static int flag_be_verbose=0;
 static int flag_check_only=0;
 
 /** help function */
@@ -64,7 +64,7 @@ void copy_file (const char * inf, const char * outf) {
  * @return 0 if success, otherwise -1
  */
 int test_plausibility (int * year, int * month, int * day, int * hour, int * min, int * sec) {
-  if (flag_be_quiet) printf ("found: y=%d m=%d d=%d h=%d m=%d s=%d\n", *year, *month, *day, *hour, *min, *sec);
+  if (flag_be_verbose) printf ("found: y=%d m=%d d=%d h=%d m=%d s=%d\n", *year, *month, *day, *hour, *min, *sec);
   if (
       1500 < *year && 
       2100 > *year &&
@@ -87,7 +87,7 @@ int test_plausibility (int * year, int * month, int * day, int * hour, int * min
 
 /** RULE0: default rule (string is correct) */
 int rule_default (char * datestring, int * year, int * month, int * day, int * hour, int * min, int * sec) {
-  if (flag_be_quiet) printf ("rule00\n");
+  if (flag_be_verbose) printf ("rule00\n");
   if (6 == sscanf(datestring, "%04d:%02d:%02d%02d:%02d:%02d", year, month, day, hour, min, sec)) {
     return test_plausibility(year, month, day, hour, min, sec);
   } else {
@@ -97,7 +97,7 @@ int rule_default (char * datestring, int * year, int * month, int * day, int * h
 
 /** RULE1: fix: '04.03.2010 09:59:17' => '04:03:2010 09:59:17' */
 int rule_ddmmyyhhmmss_01 (char * datestring, int * year, int * month, int * day, int * hour, int * min, int * sec) {
-  if (flag_be_quiet) printf ("rule01\n");
+  if (flag_be_verbose) printf ("rule01\n");
   if (6 == sscanf(datestring, "%02d.%02d.%04d%02d:%02d:%02d", day, month, year, hour, min, sec)) {
     return test_plausibility(year, month, day, hour, min, sec);
   } else {
@@ -135,7 +135,7 @@ char * correct_datestring (char * broken_datetime) {
   /* if ret is wrong, you could try another rules to apply */
   int r;
   for (r = 0; r < COUNT_OF_RULES; r++) {
-    if (flag_be_quiet) printf("Applying rule%i", r);
+    if (flag_be_verbose) printf("Applying rule%i", r);
     if (0 != (*rules_ptr[r])(broken_datetime, &year, &month, &day, &hour, &min, &sec)) {
       fprintf(stderr, "applying next rule\n");
     } else {
@@ -174,17 +174,17 @@ void fix_tiff(const char * filename) {
   uint32 count;
   int found=TIFFGetField(tif, TIFFTAG_DATETIME, &datetime, &count);
   if (1==found) { /* there exists a datetime field */
-    if (flag_be_quiet) printf("Before correction\n-----------------\n");	
-    if (flag_be_quiet) TIFFPrintDirectory(tif, stdout, TIFFPRINT_NONE);
-    if (flag_be_quiet) printf("c=%u datetime:'%s'\n", count, datetime);
+    if (flag_be_verbose) printf("Before correction\n-----------------\n");	
+    if (flag_be_verbose) TIFFPrintDirectory(tif, stdout, TIFFPRINT_NONE);
+    if (flag_be_verbose) printf("c=%u datetime:'%s'\n", count, datetime);
     /* should be corrected? */
     char * new_datetime = correct_datestring( datetime );
     if (0 != strncmp(datetime, new_datetime, TIFFDATETIMELENGTH)) {
       /* yes, correct TIFF DateTime is needed */
       if (!flag_check_only) {
         TIFFSetField(tif, TIFFTAG_DATETIME, new_datetime);
-        if (flag_be_quiet) printf("After  correction\n-----------------\n");
-        if (flag_be_quiet) TIFFPrintDirectory(tif, stdout, TIFFPRINT_NONE);
+        if (flag_be_verbose) printf("After  correction\n-----------------\n");
+        if (flag_be_verbose) TIFFPrintDirectory(tif, stdout, TIFFPRINT_NONE);
         /* write data back, only if no flag_check_only is set */
         int written = TIFFRewriteDirectory(tif);
         if (1 != written) {
@@ -192,13 +192,13 @@ void fix_tiff(const char * filename) {
           exit (-3);
         }
       } else {
-        if (flag_be_quiet) printf ("datetime correction needed\n");
+        if (flag_be_verbose) printf ("datetime correction needed\n");
       }
     } else { /* no, should not be touched */
-      if (flag_be_quiet) printf ("no correction needed\n");
+      if (flag_be_verbose) printf ("no correction needed\n");
     }
   } else if (0 == found) {
-    if (flag_be_quiet) printf ("no datetime found!\n");
+    if (flag_be_verbose) printf ("no datetime found!\n");
   }
   TIFFClose(tif);
 }
@@ -223,7 +223,7 @@ int main (int argc, char * argv[]) {
              flag_check_only = 1;
              break;
            case 'q': /* disables describing messages */
-             flag_be_quiet = 1;
+             flag_be_verbose = 1;
              break;
            case 'i': /* expects infile */
              infilename=optarg;
@@ -259,7 +259,7 @@ int main (int argc, char * argv[]) {
       exit (-9);
     }
   }
-  if (flag_be_quiet) printf ("infile='%s', outfile='%s'\n", infilename, outfilename);
+  if (flag_be_verbose) printf ("infile='%s', outfile='%s'\n", infilename, outfilename);
   if (flag_substitute_only) { /* inplace correction */
     fix_tiff(infilename);
   } else { /* source target */

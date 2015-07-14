3d0e4512e51be710ae592f5f37a446c6c6cdd086
Andreas Romeyke <andreas.romeyke@slub-dresden.de>
2015-07-14 16:39:12 +0200

    - using rules in test.c instead definig it in code in grammar-file

--- checkit/test.c
+++ checkit/test.c
@@ -1,6 +1,16 @@
-#include <stdio.h>      
+/* fixes broken TIFF Files
+ * 
+ * fixes unused tags in Baseline-TIFFs,
+ * based on http://www.awaresystems.be/imaging/tiff/tifftags/baseline.html
+ *
+ * author: Andreas Romeyke, 2013
+ * licensed under conditions of libtiff 
+ */
 
+#define YY_DEBUG
+#include <stdio.h>      
 #include <stdlib.h>
+
 int lineno=0;
 // struct to hold parsing configuration
 int lasttag = -1;
@@ -12,12 +22,22 @@ int getlineno() { return lineno;}
 int rule_tagorder_in_dsl( int tag ) {
   int prevtag = gettag();
   printf ("tag '%i' found (prev tag was: '%i')\n", tag, prevtag);
-  if (prevtag > tag)
+  if (prevtag > tag) {
     fprintf(stderr, "tag '%i' should be greater than previous tag '%i'\n", tag, prevtag);
     exit( -1 );
+  }
 }
+void tagline() {  printf("tagline\n");}
+void commentline() {  printf("commentline\n");}
+void rule_should_not_occure(char c) {
+  printf("no parser rule matched at line %i (prev tag was %i), '%c'\n", getlineno(), gettag(), c);
+}
+void rule_addtag_config() {printf( "try to match tagline at line %i\n", getlineno());
+}
+void rule_set_mandatory() { printf("tag '%i' is mandatory\n", gettag()); }
+void rule_set_optional() { printf("tag '%i' is optional\n", gettag()); }
+void rule_set_ifdepends() { printf("tag '%i' is set if depends\n", gettag()); }
 
-#define YY_DEBUG
 
 #include "config_dsl.grammar.c"   /* yyparse() */
 int main()

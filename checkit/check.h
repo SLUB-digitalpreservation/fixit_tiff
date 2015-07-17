149cb228b4c32cbf7622ae55a4c3f21b80febda1
Andreas Romeyke <andreas.romeyke@slub-dresden.de>
2015-07-17 15:30:32 +0200

    - added tif_fails() as macro (for beautified output ;))

--- checkit/check.h
+++ checkit/check.h
@@ -2,6 +2,7 @@
 #define _FIXIT_TIFF_CHECK
 #include <tiff.h>
 #include <tiffio.h>
+#define tif_fails(args...) {char str[80]; sprintf (str, args); printf("\t%s", str);}
 int check_tag_has_some_of_these_values( TIFF* tif, int tag, int count, int * values);
 int check_tag_has_value_in_range(TIFF* tif, int tag, int a, int b);
 int check_tag_has_value(TIFF* tif, int tag, int value);

54111c7cdbe3f9df131c88106728aeb86d9d546f
Andreas Romeyke <andreas.romeyke@slub-dresden.de>
2015-07-20 15:49:25 +0200

    - fixed types (LONG or SHORT) - fixed types (Undefined instead IFD)

--- checkit/check_tag_has_valid_type.c
+++ checkit/check_tag_has_valid_type.c
@@ -21,37 +21,40 @@ int check_tag_has_valid_type(TIFF* tif, int tag) {
         printf("### datatype=%i \n", datatype);
         int res = 0;
         switch (tag) {
-        case TIFFTAG_ARTIST:        res=(datatype  -  TIFF_ASCII); break;
-        case TIFFTAG_BADFAXLINES:   res=(datatype  -  TIFF_LONG); break;
-        case TIFFTAG_BITSPERSAMPLE: res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_COLORMAP:      res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_COMPRESSION:   res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_COPYRIGHT:     res=(datatype  -  TIFF_ASCII); break;
-        case TIFFTAG_DATETIME:      res=(datatype  -  TIFF_ASCII); break;
-	case TIFFTAG_DOCUMENTNAME:  res=(datatype  -  TIFF_ASCII); break;
-        case TIFFTAG_FILLORDER:     res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_HOSTCOMPUTER:  res=(datatype  -  TIFF_ASCII); break;
-        case TIFFTAG_ICCPROFILE:    res=(datatype  -  TIFF_IFD); break;		
-        case TIFFTAG_IMAGEDEPTH:    res=(datatype  -  TIFF_LONG); break;
-        case TIFFTAG_IMAGEDESCRIPTION: res=(datatype  -  TIFF_ASCII); break;
-	case TIFFTAG_IMAGELENGTH:   res=(datatype  -  TIFF_LONG); break;
-        case TIFFTAG_IMAGEWIDTH:    res=(datatype  -  TIFF_LONG); break;
-        case TIFFTAG_MAKE:          res=(datatype  -  TIFF_ASCII); break;
-        case TIFFTAG_MAXSAMPLEVALUE:res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_MINSAMPLEVALUE:res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_MODEL:         res=(datatype  -  TIFF_ASCII); break;
-        case TIFFTAG_ORIENTATION:   res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_PHOTOMETRIC:   res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_RESOLUTIONUNIT:res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_ROWSPERSTRIP:  res=(datatype  -  TIFF_LONG); break;
-	case TIFFTAG_SAMPLEFORMAT:  res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_SAMPLESPERPIXEL: res=(datatype  -  TIFF_SHORT); break;
-        case TIFFTAG_SOFTWARE:      res=(datatype  -  TIFF_ASCII); break;
-        case TIFFTAG_XRESOLUTION:   res=(datatype  -  TIFF_RATIONAL); break;
-        case TIFFTAG_YRESOLUTION:   res=(datatype  -  TIFF_RATIONAL); break;
-        default: res = 0;
+        case TIFFTAG_ARTIST:            res=(datatype  ==  TIFF_ASCII); break;
+        case TIFFTAG_BADFAXLINES:       res=(datatype  ==  TIFF_LONG); break;
+        case TIFFTAG_BITSPERSAMPLE:     res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_COLORMAP:          res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_COMPRESSION:       res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_COPYRIGHT:         res=(datatype  ==  TIFF_ASCII); break;
+        case TIFFTAG_DATETIME:          res=(datatype  ==  TIFF_ASCII); break;
+	case TIFFTAG_DOCUMENTNAME:      res=(datatype  ==  TIFF_ASCII); break;
+        case TIFFTAG_FILLORDER:         res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_HOSTCOMPUTER:      res=(datatype  ==  TIFF_ASCII); break;
+        case TIFFTAG_ICCPROFILE:        res=(datatype  ==  TIFF_UNDEFINED); break;		
+        case TIFFTAG_IMAGEDEPTH:        res=(datatype  ==  TIFF_LONG); break;
+        case TIFFTAG_IMAGEDESCRIPTION:  res=(datatype  ==  TIFF_ASCII); break;
+	case TIFFTAG_IMAGELENGTH:       res=(datatype  ==  TIFF_LONG) || (datatype ==  TIFF_SHORT);; break;
+        case TIFFTAG_IMAGEWIDTH:        res=(datatype  ==  TIFF_LONG) || (datatype ==  TIFF_SHORT);; break;
+        case TIFFTAG_MAKE:              res=(datatype  ==  TIFF_ASCII); break;
+        case TIFFTAG_MAXSAMPLEVALUE:    res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_MINSAMPLEVALUE:    res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_MODEL:             res=(datatype  ==  TIFF_ASCII); break;
+        case TIFFTAG_ORIENTATION:       res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_PHOTOMETRIC:       res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_RESOLUTIONUNIT:    res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_ROWSPERSTRIP:      res=(datatype  ==  TIFF_LONG) || (datatype ==  TIFF_SHORT); break;
+	case TIFFTAG_SAMPLEFORMAT:      res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_SAMPLESPERPIXEL:   res=(datatype  ==  TIFF_SHORT); break;
+        case TIFFTAG_SOFTWARE:          res=(datatype  ==  TIFF_ASCII); break;
+        case TIFFTAG_STRIPBYTECOUNTS:   res=(datatype  ==  TIFF_LONG) || (datatype ==  TIFF_SHORT); break;
+        case TIFFTAG_STRIPOFFSETS:      res=(datatype  ==  TIFF_LONG) || (datatype ==  TIFF_SHORT); break;
+        case TIFFTAG_XRESOLUTION:       res=(datatype  ==  TIFF_RATIONAL); break;
+        case TIFFTAG_YRESOLUTION:       res=(datatype  ==  TIFF_RATIONAL); break;
+
+        default: res = 1;
         }
-        if (res > 0) {
+        if (!res) {
           tif_fails("tag %i has incorrect type: %i\n", tag, datatype);
           return 1;
         } else {

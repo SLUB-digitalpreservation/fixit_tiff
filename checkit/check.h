c1a7d2fd8172cd45f84f2f26e18a3a851a9f282b
Andreas Romeyke <andreas.romeyke@slub-dresden.de>
2015-07-20 11:48:00 +0200

    - added check_tag_has_valuelist() - added tagname in reporting

--- checkit/check.h
+++ checkit/check.h
@@ -4,6 +4,7 @@
 #include <tiffio.h>
 #define tif_fails(args...) {char str[80]; sprintf (str, args); printf("\t%s", str);}
 int check_tag_has_some_of_these_values( TIFF* tif, int tag, int count, int * values);
+int check_tag_has_valuelist( TIFF* tif, int tag, int count, int * values);
 int check_tag_has_value_in_range(TIFF* tif, int tag, int a, int b);
 int check_tag_has_value(TIFF* tif, int tag, int value);
 int check_tag(TIFF* tif, int tag);
--- checkit/check_tag_has_value.c
+++ checkit/check_tag_has_value.c
@@ -3,7 +3,7 @@
 #include "../fixit/tiff_helper.h"
 
 int check_tag_has_value(TIFF* tif, int tag, int value) {
-  printf("check if tag %i has value %i\n", tag, value);
+  printf("check if tag %i (%s) has value %i\n", tag, TIFFFieldName(TIFFFieldWithTag(tif, tag)), value);
   if (NULL == tif) {
     fprintf( stderr, "TIFF pointer is empty\n");
     tif_fails("TIFF pointer is empty\n");
@@ -27,4 +27,44 @@ int check_tag_has_value(TIFF* tif, int tag, int value) {
   }
   return 1;
 }
+
+int check_tag_has_valuelist(TIFF* tif, int tag, int count, int * values) {
+  printf("check if tag %i (%s) has these %i-values", tag, TIFFFieldName(TIFFFieldWithTag(tif, tag)), count);
+  int i;
+  int * p = values;
+  for (i=0; i< count; i++) {
+    printf (", %i", *p);
+    p++;
+  }
+  printf("\n");
+  uint16 val;
+  void * data;
+  int found=TIFFGetField(tif, tag, &val, data);
+  if (1==found) { /* check va-list */
+    printf("### found: value=%i(%x) data=%p \n",val,val, data);
+    /* we check only count, because we evaluate only int-values */
+    p = values;
+    int has_error=0;
+    for (i=0; i< count; i++) {
+      printf("### value = %i", *p);
+      if ((val == *p) && (NULL == data)) {
+      } else {
+      printf("error\n");
+        has_error++; 
+      }
+      p++;
+    }
+    if (has_error > 0) {
+      tif_fails("tag %i should have values, but have count/value=%i(%x) data=%p\n", tag, val, val, data);
+      return 1;
+    } else { /* no error */
+      return 0;
+    }
+  } else { /* tag not defined */ 
+    tif_fails("tag %i should exist, because defined\n", tag);
+    return 1;
+  }
+  return 1;
+}
+
 
